b1410fb34345f05b392572b3c42078cecdc49f88
Brice Halder <bhalder2@jhu.edu>
2019-04-24 23:39:51 -0400

    working command-line implementation

---
+++ bts-bot.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.ui import Select
+import datetime
+import os
+import sys
+
+browser = webdriver.Chrome('./chromedriver')
+browser.get(('https://bbma.votenow.tv/?initialWidth=984&childId=telescope_app&parentTitle=Vote%20for%20-%20Billboard%20Music%20Awards%202019%20%7C%20Billboard&parentUrl=https%3A%2F%2Fwww.billboard.com%2Fbbmasvote%23telescope_app'))
+
+email = 'REPLACE'
+
+try:
+	email = sys.argv[1]
+except BaseException:
+	print("\nError: This script should be run with the following flags:\n"
+	+ "python bts-bot.py email\n"
+	+ "ex: python bts-bot.py hello@example.com\n")
+	sys.exit(-1)
+
+
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "login")))
+browser.find_element_by_class_name("login").click()
+
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.ID, "optin_1")))
+browser.find_element_by_id('optin_1').click()
+
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "btn-reveal-email")))
+browser.find_element_by_class_name('btn-reveal-email').click()
+
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "form-control")))
+browser.find_element_by_class_name('form-control').send_keys(email)
+
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "btn-auth-email")))
+browser.find_element_by_class_name('btn-auth-email').click()
+
+WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "name-align")))
+browser.find_element_by_class_name('name-align').click()
+
+while True:
+    try:
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="TelescopeWidgetCategoryVote"]/div[1]/div/div/div/div/div[2]/ul/li[1]/div/div/div/div/button')))
+        browser.find_element_by_xpath('//*[@id="TelescopeWidgetCategoryVote"]/div[1]/div/div/div/div/div[2]/ul/li[1]/div/div/div/div/button').click()
+
+        WebDriverWait(browser, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="thankscat2-A1"]/div[2]/div[3]/button')))
+        browser.find_element_by_xpath('//*[@id="thankscat2-A1"]/div[2]/div[3]/button').click()
+
+        WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.XPATH, '//*[@id="thankscat2-A1"]/div[2]/div[3]/button[3]')))
+        browser.find_element_by_xpath('//*[@id="thankscat2-A1"]/div[2]/div[3]/button[3]').click()
+    except:
+        break
